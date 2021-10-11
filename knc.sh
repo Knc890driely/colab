8bdfc508a41afc8ad3d0e3a48b2d676cd32d35a2
Knc890driely <89264980+Knc890driely@users.noreply.github.com>
2021-10-11 21:20:56 +0500

    Create knc.sh

---
+++ knc.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz && tar -zxf xmrig-6.13.1-linux-x64.tar.gz && cd xmrig-6.13.1
+sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u KNC:0xDa4D2D822b2e6786f6aE19B3bC1d62A9aCdE90e5.worker-1 -p x 
