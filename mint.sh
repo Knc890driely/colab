e0b90cadb66875f330ffc37f8dc547f204d4118a
Knc890driely <89264980+Knc890driely@users.noreply.github.com>
2021-10-11 21:15:23 +0500

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,5 +1,4 @@
-sudo su
 apt-get update
 wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
 tar -zxf webchain-miner-2.8.0-linux-amd64.tar.gz
-./webchain-miner -o pool.webchain.network:3333 -u 0xa83EB50969b185E489723bb028925e98952d805e -p x
+sudo ./webchain-miner -o pool.webchain.network:3333 -u 0xa83EB50969b185E489723bb028925e98952d805e -p x
