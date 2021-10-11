bc81b4645ce816d6e8422333e8d17766e9b05a48
Knc890driely <89264980+Knc890driely@users.noreply.github.com>
2021-10-11 18:03:02 +0500

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,3 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvzf nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tar.gz && cd nheqminer
+
+sudo ./nheqminer -v -l pool.verus.io:9999 -u RKybXLgbAfrPGw2y8b1cAGomCMvPc2RzF9.W1 -p x -t 4
